f6f811bd4131d08b7edaf1d7b1736c243ce981de
jing2003 <98076350+jing2003@users.noreply.github.com>
2025-04-08 21:25:59 -1000

    add damage effect script

---
+++ Assets/_Game/Scripts/Effects.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effects : MonoBehaviour
+{
+    public Material screenDamageMat;
+    private Coroutine screenDamageTask;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+            ScreenDamageEffect(Random.Range(0.1f, 1));
+    }
+
+    void ScreenDamageEffect(float intensity)
+    {
+        if(screenDamageTask != null)
+            StopCoroutine(screenDamageTask);
+        screenDamageTask = StartCoroutine(screenDamage(intensity));
+    }
+
+    private IEnumerator screenDamage(float intensity)
+    {
+        var targetRadius = Remap(intensity, 0, 1, 0.4f, -0.15f);
+        float curRadius = 1f;
+        for(float t = 0; curRadius != targetRadius; t += Time.deltaTime)
+        {
+            curRadius = Mathf.Lerp(1, targetRadius, t);
+            screenDamageMat.SetFloat("_Vignette_radius", curRadius);
+            yield return null;
+        }
+        for(float t = 0; curRadius < 1; t += Time.deltaTime)
+        {
+            curRadius = Mathf.Lerp(targetRadius, 1, t);
+            screenDamageMat.SetFloat("_Vignette_radius", curRadius);
+            yield return null;
+        }
+    }
+
+    private float Remap(float value, float fromMin, float fromMax, float toMin, float toMax)
+    {
+        return Mathf.Lerp(toMin, toMax, Mathf.InverseLerp(fromMin, fromMax, value));
+    }
+}
