21d29fe9f9bfb04fd1d10da4e6f7aa83a45b632d
jing2003 <98076350+jing2003@users.noreply.github.com>
2025-04-16 16:49:39 -1000

    updates

--- Assets/Scripts/Effects.cs
+++ Assets/Scripts/Effects.cs
@@ -7,15 +7,23 @@ public class Effects : MonoBehaviour
     public Material screenDamageMat;
     private Coroutine screenDamageTask;
 
-    private void Update()
+    private void Start()
     {
-        if(Input.GetKeyDown(KeyCode.T))
-            ScreenDamageEffect(Random.Range(0.1f, 1));
+        screenDamageMat.SetFloat("_Vignette_radius", 1f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Alien"))
+        {
+            float damageIntensity = Random.Range(0.1f, 1f); // You can adjust this
+            ScreenDamageEffect(damageIntensity);
+        }
     }
 
     void ScreenDamageEffect(float intensity)
     {
-        if(screenDamageTask != null)
+        if (screenDamageTask != null)
             StopCoroutine(screenDamageTask);
         screenDamageTask = StartCoroutine(screenDamage(intensity));
     }
@@ -24,15 +32,16 @@ public class Effects : MonoBehaviour
     {
         var targetRadius = Remap(intensity, 0, 1, 0.4f, -0.15f);
         float curRadius = 1f;
-        for(float t = 0; curRadius != targetRadius; t += Time.deltaTime)
+        for (float t = 0; curRadius != targetRadius; t += Time.deltaTime)
         {
             curRadius = Mathf.Lerp(1, targetRadius, t);
             screenDamageMat.SetFloat("_Vignette_radius", curRadius);
             yield return null;
         }
-        for(float t = 0; curRadius < 1; t += Time.deltaTime)
+
+        for (float t = 0; curRadius < 1f; t += Time.deltaTime)
         {
-            curRadius = Mathf.Lerp(targetRadius, 1, t);
+            curRadius = Mathf.Lerp(targetRadius, 1f, t);
             screenDamageMat.SetFloat("_Vignette_radius", curRadius);
             yield return null;
         }
@@ -43,3 +52,50 @@ public class Effects : MonoBehaviour
         return Mathf.Lerp(toMin, toMax, Mathf.InverseLerp(fromMin, fromMax, value));
     }
 }
+
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Effects : MonoBehaviour
+// {
+//     public Material screenDamageMat;
+//     private Coroutine screenDamageTask;
+
+//     private void Update()
+//     {
+//         if(Input.GetKeyDown(KeyCode.T))
+//             ScreenDamageEffect(Random.Range(0.1f, 1));
+//     }
+
+//     void ScreenDamageEffect(float intensity)
+//     {
+//         if(screenDamageTask != null)
+//             StopCoroutine(screenDamageTask);
+//         screenDamageTask = StartCoroutine(screenDamage(intensity));
+//     }
+
+//     private IEnumerator screenDamage(float intensity)
+//     {
+//         var targetRadius = Remap(intensity, 0, 1, 0.4f, -0.15f);
+//         float curRadius = 1f;
+//         for(float t = 0; curRadius != targetRadius; t += Time.deltaTime)
+//         {
+//             curRadius = Mathf.Lerp(1, targetRadius, t);
+//             screenDamageMat.SetFloat("_Vignette_radius", curRadius);
+//             yield return null;
+//         }
+//         for(float t = 0; curRadius < 1; t += Time.deltaTime)
+//         {
+//             curRadius = Mathf.Lerp(targetRadius, 1, t);
+//             screenDamageMat.SetFloat("_Vignette_radius", curRadius);
+//             yield return null;
+//         }
+//     }
+
+//     private float Remap(float value, float fromMin, float fromMax, float toMin, float toMax)
+//     {
+//         return Mathf.Lerp(toMin, toMax, Mathf.InverseLerp(fromMin, fromMax, value));
+//     }
+// }
